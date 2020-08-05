956b1e6fc0912b3fab4a661b2a23c29e4983e827
denghuaguang <denghuaguang@bytedance.com>
2020-08-05 11:20:54 +0800

    feat: add rsa sign
    Change-Id: I3ba58a3e56a185fd12f1310c235bd2a52908c985

---
+++ rsa/rsa.go
@@ -0,0 +1,92 @@
+package ticket
+
+import (
+	"crypto"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha1"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/hex"
+	"encoding/pem"
+	"fmt"
+	"os"
+)
+
+//genRsaKey RSA公钥私钥产生
+func genRsaKey(bits int) error {
+	// 生成私钥文件
+	privateKey, err := rsa.GenerateKey(rand.Reader, bits)
+	if err != nil {
+		return err
+	}
+
+	derStream := x509.MarshalPKCS1PrivateKey(privateKey)
+	block := &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: derStream,
+	}
+	file, err := os.Create("private.pem")
+	if err != nil {
+		return err
+	}
+	err = pem.Encode(file, block)
+	if err != nil {
+		return err
+	}
+	// 生成公钥文件
+	publicKey := &privateKey.PublicKey
+	derPkix := x509.MarshalPKCS1PublicKey(publicKey)
+
+	block = &pem.Block{
+		Type:  "PUBLIC KEY",
+		Bytes: derPkix,
+	}
+	file, err = os.Create("public.pem")
+	if err != nil {
+		return err
+	}
+	err = pem.Encode(file, block)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func RsaSignWithSha1Hex(data string, prvKey string) (string, error) {
+	keyByts, err := hex.DecodeString(prvKey)
+	if err != nil {
+		fmt.Println(err)
+		return "", err
+	}
+	privateKey, err := x509.ParsePKCS8PrivateKey(keyByts)
+	if err != nil {
+		fmt.Println("ParsePKCS8PrivateKey err", err)
+		return "", err
+	}
+	h := sha1.New()
+	h.Write([]byte([]byte(data)))
+	hash := h.Sum(nil)
+	signature, err := rsa.SignPKCS1v15(rand.Reader, privateKey.(*rsa.PrivateKey), crypto.SHA1, hash[:])
+	if err != nil {
+		fmt.Printf("Error from signing: %s\n", err)
+		return "", err
+	}
+	out := hex.EncodeToString(signature)
+	return out, nil
+}
+
+func RsaVerySignWithSha1Base64(originalData, signData, pubKey string) error {
+	sign, err := base64.StdEncoding.DecodeString(signData)
+	if err != nil {
+		return err
+	}
+	public, _ := base64.StdEncoding.DecodeString(pubKey)
+	pub, err := x509.ParsePKIXPublicKey(public)
+	if err != nil {
+		return err
+	}
+	hash := sha1.New()
+	hash.Write([]byte(originalData))
+	return rsa.VerifyPKCS1v15(pub.(*rsa.PublicKey), crypto.SHA1, hash.Sum(nil), sign)
+}
---
+++ rsa/rsa_test.go
@@ -0,0 +1,12 @@
+package ticket
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRsa(t *testing.T) {
+	genRsaKey(1024)
+	id_id := 1
+	fmt.Println(id_id)
+}
